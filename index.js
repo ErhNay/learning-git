697ba92801824be253e6e3f22680ce4ad6a59ee6
ErhNay <erhnay86@gmail.com>
2022-08-04 07:35:31 +0000

    Response to an inquiry made

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello, Git and GitHub");
 console.log("Learning Git and GitHub with Amigos Code");
 console.log("This is going so well my guy");
+console.log("Yes,there is. Please resove this ASAP");
